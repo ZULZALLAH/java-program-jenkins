a088fdaf92410e46012c220a2fb852301ed6214b
ZULZALLAH <saifahmadkhan02@gmail.com>
2024-04-01 08:56:47 +0530

    Initial Commit

---
+++ 11am-batch/Desktop/java program jenkins/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main {
+public static String x="";
+    public static void main(String[] args) {
+        System.out.println(x + "This is java program");
+        System.out.println(x + "output from java program");
+        System.out.println(new Date());
+        System.out.println("This is Git Desktop");
+    }
+}
